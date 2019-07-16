ec8e140c8f4916fc97af6054401fa1b878563b23
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-07-16 13:23:00 +0100

    countries and supportedCards objects added

---
+++ script.js
@@ -0,0 +1,52 @@
+const supportedCards = {
+    visa: 'img\visa.png',
+    mastercard: 'img\mastercard.png'
+
+  };
+
+  const countries = [{
+      code: "US",
+      currency: "USD",
+      country: 'United States'
+    },
+    {
+      code: "NG",
+      currency: "NGN",
+      country: 'Nigeria'
+    },
+    {
+      code: 'KE',
+      currency: 'KES',
+      country: 'Kenya'
+    },
+    {
+      code: 'UG',
+      currency: 'UGX',
+      country: 'Uganda'
+    },
+    {
+      code: 'RW',
+      currency: 'RWF',
+      country: 'Rwanda'
+    },
+    {
+      code: 'TZ',
+      currency: 'TZS',
+      country: 'Tanzania'
+    },
+    {
+      code: 'ZA',
+      currency: 'ZAR',
+      country: 'South Africa'
+    },
+    {
+      code: 'CM',
+      currency: 'XAF',
+      country: 'Cameroon'
+    },
+    {
+      code: 'GH',
+      currency: 'GHS',
+      country: 'Ghana'
+    }
+  ];
